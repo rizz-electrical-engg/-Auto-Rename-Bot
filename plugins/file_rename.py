9d5ebe24be932ae7e02894e4c4021d3e0058049a
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-10-15 20:15:02 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -287,9 +287,3 @@ async def auto_rename_files(client, message):
         del renaming_operations[file_id]
 
 
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
